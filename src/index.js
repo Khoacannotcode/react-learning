c70e0ed269f995e67d2f8835ffd62c4c7058d6b3
Khoacannotcode <18520929@gm.uit.edu.vn>
2021-11-02 14:45:44 +0700

    modify index...

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
+
+const activities = [
+  {
+    timestamp: new Date().getTime(),
+    text: "Ate lunch",
+    user: {
+      id: 1, name: 'Nate',
+      avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+    },
+    comments: [{ from: 'Ari', text: 'Me too!' }]
+  },
+  {
+    timestamp: new Date().getTime(),
+    text: "Woke up early for a beautiful run",
+    user: {
+      id: 2, name: 'Ari',
+      avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+    },
+    comments: [{ from: 'Nate', text: 'I am so jealous' }]
+  },
+]
+
+
 class Root extends React.Component {
   render() {
     return (
@@ -11,13 +34,12 @@ class Root extends React.Component {
           <Header title="Profile" />
           <Header title="Settings" />
           <Header title="Chat" />
-          <Content />
+          <Content activities={activities} />
         </div>
       </div>
     );
   }
 }
-
 
 class Header extends React.Component {
   render() {
@@ -44,30 +66,46 @@ class Header extends React.Component {
   }
 }
 
+class ActivityItem extends React.Component {
+  render() {
+    const {activity} = this.props;
+
+    return (
+      <div className="item">
+        <div className="avatar">
+          <img
+            alt= {activity.text}
+            src= {activity.user.avatar}
+          />
+          {/* Doug */}
+        </div>
+
+        <span className="time">
+          {activity.timestamp}
+        </span>
+        <p>{activity.text}</p>
+        <div className="commentCount">
+          {activity.comments.length}
+        </div>
+      </div>
+    );
+  }
+}
+
+
 class Content extends React.Component {
   render() {
-    return (
+    const {activities} = this.props; //ES6 destructing
+    return (      
       <div className="content">
         <div className="line"></div>
 
-        {/* Timeline item */}
-        <div className="item">
-          <div className="avatar">
-            <img
-              alt="Doug"
-              src="http://www.croop.cl/UI/twitter/images/doug.jpg"
-            />
-            {/* Doug */}
-          </div>
-
-          <span className="time">An hour ago</span>
-          <p>Ate lunch</p>
-          <div className="commentCount">2</div>
-        </div>
-
-        {/* ... */}
+          {/* Use map function to loop each activity */}
+          {activities.map((activity) => (
+            <ActivityItem activity={activity} />
+          ))}
       </div>
-    );
+    )
   }
 }
 var mount = document.querySelector('#root');
